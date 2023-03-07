6fb1e34abc956a67b480b9161129a724063ec1a9
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-03-07 08:35:25 -0600

    installed dependencies, required modules, create connection to db

--- Main/server.js
+++ Main/server.js
@@ -1,2 +1,18 @@
+// Require packages
 const express = require('express');
-const mysql = require('')
+const mysql = require('mysql2');
+
+const app = express(); // Require the Express module and assign to variable
+const port = 3000;
+
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: ''
+});
+
+db.connect((err) => {
+    if (err) throw err; // Display error when cannot connect to db. 
+    console.log("Connection established!");
+});
