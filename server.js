b55a6ea70900991087b9c3f196026a700dde955d
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-03-10 08:25:12 -0600

    this commit installs the  inquirer package and saves, creates switch statements for the choices selected from the inquirer prompt

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const cTable = require("console.table");
 const inquirer = require("inquirer");
 
-// Create a connection to Database
+// Create a connection to database
 const db = mysql.createConnection(
   {
     host: "127.0.0.1",
@@ -26,22 +26,60 @@ function init() {
       type: "list",
       name: "options",
       message: "help",
-      choices: [],
+      choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
     })
     .then((res) => {
-      // think about a swich statment to chekc against all the options
-    });
+      // think about a swich statment to check against all the options
+      console.log(res.options);
+
+      // Do a query based on the user selection
+      switch(res.options) {
+        case 'View all departments':
+          db.query('SELECT * from department;', (err, result) => {
+            if (err) {
+                console.log(err, 'System error')
+            }
+            // Display the department table to the user in the terminal
+            console.table(result)
+          });
+          break;
+          // WHEN I choose to view all roles
+          // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+        case 'View all roles':
+          db.query('SELECT * FROM role AND', (err, result) => {
+            if (err) {
+              console.log(err, 'System error')
+            }
+            console.table(result)
+          });
+          break;
+          // WHEN I choose to view all employees
+          // THEN I am presented with a formatted table showing employee data, including employee ids,
+          // first names, last names, job titles, departments, salaries, and 
+          // managers that the employees report to
+        case 'View all employees':
+          db.query('', (err, result) => {
+            if (err) {
+              console.log(err, 'System error')
+            }
+          });
+          console.table(result)
+          break;
+          // WHEN I choose to add a department
+          // THEN I am prompted to enter the name of the department and that department is added to the database
+        case 'Add a department':
+          db.query('')
+        default:
+          console.log('System error');
+      }
+    }).catch((err) => console.error(err));
+    
 }
 
+// Invoke the init() function upon startup
 init();
 
 // WHEN I choose to view all departments
 // THEN I am presented with a formatted table showing department names and department ids
 
-// db.query("SELECT * from department;", (err, result) => {
-//   if (err) {
-//       console.log(err)
-//   }
-//   // do something with the result
-//   console.table(result)
-// });
+
