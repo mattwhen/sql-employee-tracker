9e8685088f4114eef938eb4f6020f313ba1adf44
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-03-11 22:52:18 -0600

    this commit will add more prompts to the server.js file and creating a separate file prompt.js to organize the code more effectively

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// Require packages
 const mysql = require("mysql2");
 const cTable = require("console.table");
 const inquirer = require("inquirer");
-
+const addRole = require('./prompt');
 // Create a connection to database
 const db = mysql.createConnection(
   {
@@ -11,7 +10,7 @@ const db = mysql.createConnection(
     password: "",
     database: "employee_db",
   },
-  console.log("connected to the db")
+  console.log("What would you like to do?")
 );
 
 db.connect((err) => {
@@ -26,7 +25,7 @@ function init() {
       type: "list",
       name: "options",
       message: "help",
-      choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
+      choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role", "Quit application"],
     })
     .then((res) => {
       // think about a swich statment to check against all the options
@@ -41,16 +40,19 @@ function init() {
             }
             // Display the department table to the user in the terminal
             console.table(result)
+            init();
           });
           break;
           // WHEN I choose to view all roles
           // THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
         case 'View all roles':
-          db.query('SELECT * FROM role AND', (err, result) => {
+          db.query('SELECT * FROM role', (err, result) => {
             if (err) {
               console.log(err, 'System error')
             }
             console.table(result)
+            console.log("What would you like to do?")
+            init();
           });
           break;
           // WHEN I choose to view all employees
@@ -64,22 +66,59 @@ function init() {
             }
           });
           console.table(result)
+          console.log("What would you like to do?")
+          init();
           break;
           // WHEN I choose to add a department
           // THEN I am prompted to enter the name of the department and that department is added to the database
         case 'Add a department':
-          db.query('')
-        default:
-          console.log('System error');
+          addDepartment();
+          break;
+        case 'Add a role':
+          addRole();
+          break;
+        case 'Add an employee':
+          addEmployee();
+          break;
+        case 'Update an Employee role':
+          updateEmployee();
+          break;
+        // case 'Quit application':
+        //   console.log('Exiting application...');
+        //   const prompt = inquirer.prompt({
+          //     input: 'input',
+          //     name: 'quitApp'
+          //   });
+          //   prompt.ui.close();
+          //   break;
+          default:
+            console.log('System error');
       }
     }).catch((err) => console.error(err));
-    
 }
+// When the 'Add a department' choice is selected, prompt the user to enter name of department:
+  function addDepartment() {
+    inquirer
+    .prompt({
+      type: 'input',
+      name: 'departmentName',
+      message: 'Enter the name of the department',
+    })
+    .then((response) => {
+      db.query(
+      `INSERT INTO department (name)
+       VALUES ('${response.departmentName}');`)
+       console.log(`Added ${response.departmentName} to the database`)
+       console.log("What would you like to do?")
+       init();
+    })
+    .catch((err) => console.error(err));
+  };
+
+  function addEmployee() {
+
+  };
 
 // Invoke the init() function upon startup
 init();
-
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-
 
